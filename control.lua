47a1a24f40a626f27446a85c3592ed1e88998d73
ZwerOxotnik <zweroxotnik@gmail.com>
2022-05-22 09:14:50 +0300

    Update to 0.9.10

--- control.lua
+++ control.lua
@@ -7,6 +7,9 @@ local modules = {}
 modules.better_commands = require("models/BetterCommands/control")
 modules.EasyAPI = require("models.EasyAPI")
 
+
 modules.better_commands:handle_custom_commands(modules.EasyAPI) -- adds commands
 
 event_handler.add_libraries(modules)
+
+if script.active_mods["gvv"] then require("__gvv__.gvv")() end
